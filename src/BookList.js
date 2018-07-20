b181339fa9954fa26335b463a8134a2ecc0c31b0
kes2401 <keithscully83@gmail.com>
2018-07-20 23:21:24 +0100

    FEATURE: added router

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function BookList(props) {
 
@@ -158,7 +159,7 @@ function BookList(props) {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to='/search'>Add a book</Link>
             </div>
           </div>
 }
